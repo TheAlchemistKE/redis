f6b667f4812a10e5cec387f56d957fe5752f6c5f
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2025-01-28 22:01:36 +0300

    feat: add ECHO command and RESP protocol parser
    - Implement RESP protocol parser supporting arrays and bulk strings
    - Add support for ECHO command
    - Make command handling case-insensitive
    - Structure code for future command additions

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,135 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// RESP Types
+enum RespType {
+    SimpleString = '+',
+    Error = '-',
+    Integer = ':',
+    BulkString = '$',
+    Array = '*'
+}
 
-const server: net.Server = net.createServer((connection: net.Socket) => {
-    // Listen for data on the connection
-    connection.on('data', (_data: Buffer) => {
-        // Send PONG response in RESP format
-        const response = "+PONG\r\n";
-        connection.write(response);
-    });
-});
+class RespParser {
+    private data: string = '';
+    private position: number = 0;
 
+    constructor(data: string) {
+        this.data = data;
+    }
+
+    parse(): any {
+        const type = this.data[this.position];
+        this.position++;
+
+        switch (type) {
+            case RespType.Array:
+                return this.parseArray();
+            case RespType.BulkString:
+                return this.parseBulkString();
+            case RespType.SimpleString:
+                return this.parseSimpleString();
+            default:
+                throw new Error(`Unsupported RESP type: ${type}`);
+        }
+    }
+
+    private parseArray(): string[] {
+        const length = this.parseInteger();
+        const result: string[] = [];
+
+        for (let i = 0; i < length; i++) {
+            this.consumeCRLF();
+            result.push(this.parse());
+        }
+
+        return result;
+    }
+
+    private parseBulkString(): string {
+        const length = this.parseInteger();
+        if (length === -1) {
+            return ''; // Null bulk string
+        }
+
+        this.consumeCRLF();
+        const result = this.data.slice(this.position, this.position + length);
+        this.position += length;
+        this.consumeCRLF();
+        return result;
+    }
+
+    private parseSimpleString(): string {
+        let result = '';
+        while (this.position < this.data.length && this.data[this.position] !== '\r') {
+            result += this.data[this.position];
+            this.position++;
+        }
+        this.consumeCRLF();
+        return result;
+    }
+
+    private parseInteger(): number {
+        let numStr = '';
+        while (this.position < this.data.length && this.data[this.position] !== '\r') {
+            numStr += this.data[this.position];
+            this.position++;
+        }
+        this.consumeCRLF();
+        return parseInt(numStr, 10);
+    }
+
+    private consumeCRLF() {
+        if (this.data[this.position] === '\r' && this.data[this.position + 1] === '\n') {
+            this.position += 2;
+        }
+    }
+}
+
+class RedisServer {
+    private server: net.Server;
+
+    constructor() {
+        this.server = net.createServer((connection: net.Socket) => {
+            this.handleConnection(connection);
+        });
+    }
+
+    private handleConnection(connection: net.Socket) {
+        connection.on('data', (data: Buffer) => {
+            try {
+                const parser = new RespParser(data.toString());
+                const command = parser.parse();
+                const response = this.handleCommand(command);
+                connection.write(response);
+            } catch (error) {
+                console.error('Error processing command:', error);
+                connection.write('-ERR Invalid command format\r\n');
+            }
+        });
+    }
+
+    private handleCommand(command: string[]): string {
+        const commandName = command[0].toUpperCase();
+        
+        switch (commandName) {
+            case 'PING':
+                return '+PONG\r\n';
+            case 'ECHO':
+                if (command.length < 2) {
+                    return '-ERR wrong number of arguments for \'echo\' command\r\n';
+                }
+                const arg = command[1];
+                return `$${arg.length}\r\n${arg}\r\n`;
+            default:
+                return `-ERR unknown command '${commandName}'\r\n`;
+        }
+    }
+
+    listen(port: number, host: string) {
+        this.server.listen(port, host);
+    }
+}
+
+// Start the server
+const server = new RedisServer();
 server.listen(6379, "127.0.0.1");
